3ceb3451ce3fdced8175d2aed97213784528b904
Algo Tech <tairocruise@yahoo.com>
2023-08-10 15:50:40 +0300

    stable

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'tairo/c2b-test',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '55b31656ac749c5492e22cc7f4b004757ceef1c2',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'phpseclib/phpseclib' => array(
+            'pretty_version' => '2.0.38',
+            'version' => '2.0.38.0',
+            'reference' => 'b03536539f43a4f9aa33c4f0b2f3a1c752088fcd',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpseclib/phpseclib',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'tairo/c2b-test' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '55b31656ac749c5492e22cc7f4b004757ceef1c2',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
